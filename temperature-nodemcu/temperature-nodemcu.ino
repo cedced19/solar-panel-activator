5e3e4a64560912780d635cbeb6a1102d4a7154f7
Cédric JUNG <cedced19@gmail.com>
2023-03-25 14:52:54 +0100

    Add implementation on nodemcu

--- temperature-nodemcu/temperature-nodemcu.ino
+++ temperature-nodemcu/temperature-nodemcu.ino
@@ -1,30 +1,43 @@
 // https://www.electronicwings.com/nodemcu/thermistor-interfacing-with-nodemcu
 
-const double VCC = 3.3;             // NodeMCU on board 3.3v vcc
-const double R2 = 10000;            // 10k ohm series resistor
-const double adc_resolution = 1023; // 10-bit adc
-
-const double A = 0.001129148;   // thermistor equation parameters
-const double B = 0.000234125;
-const double C = 0.0000000876741; 
+// which analog pin to connect
+#define THERMISTORPIN A0         
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 10000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 10000    
 
 void setup() {
   Serial.begin(9600);  /* Define baud rate for serial communication */
 }
 
 void loop() {
-  double Vout, Rth, temperature, adc_value; 
+  float measure;
+  float temperature;
 
-  adc_value = analogRead(A0);
-  Vout = (adc_value * VCC) / adc_resolution;
-  Rth = (VCC * R2 / Vout) - R2;
+  measure =  analogRead(THERMISTORPIN);
 
-/*  Steinhart-Hart Thermistor Equation:
- *  Temperature in Kelvin = 1 / (A + B[ln(R)] + C[ln(R)]^3)
- *  where A = 0.001129148, B = 0.000234125 and C = 8.76741*10^-8  */
-  temperature = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)),3))));   // Temperature in kelvin
+  Serial.print("measure analog reading "); 
+  Serial.println(measure);
+  
+  // convert the value to resistance
+  measure = 1023 / measure - 1;
+  measure = SERIESRESISTOR / measure;
+  Serial.print("Thermistor resistance "); 
+  Serial.println(measure);
+  
+  float steinhart;
+  steinhart = measure / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
 
-  temperature = temperature - 273.15;  // Temperature in degree celsius
+  temperature = steinhart - 273.15;  // Temperature in degree celsius
   Serial.print("Temperature = ");
   Serial.print(temperature);
   Serial.println(" degree celsius");
